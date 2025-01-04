a63ce2653ef5f4a817dcf6cd424d1a897aedc376
tovacinni <tovacinni@gmail.com>
2025-01-04 08:09:06 -0800

    UI test

--- src/data/publication.ts
+++ src/data/publication.ts
@@ -20,7 +20,7 @@ export const publicationData: Publication[] = [
     authors: "Jane Smith, Sarah Johnson, Yue Zhang",
     paperUrl: "https://arxiv.org/abs/2409.15476",
     codeUrl: "https://github.com/jsmith/scalable-causal-discovery",
-    bibtex: "https://arxiv.org/abs/2409.15476.bib",
+    //bibtex: "https://arxiv.org/abs/2409.15476.bib",
     tldr: "Using causal discovery to find the causal structure of high-dimensional time series data.",
     imageUrl:
       "https://images.unsplash.com/photo-1561622539-dffbfc2008fd?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
